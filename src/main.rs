383846257006956886511f8c2924efcc86ae3028
Alexander Hill <me@alxhill.com>
2025-11-05 22:11:26 -0500

    flashy boi!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
+use embedded_hal::i2c::I2c;
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -8,20 +10,22 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    let mut led = pins.d13.into_output();
+    let mut i2c = arduino_hal::I2c::new(
+        dp.TWI,
+        pins.a4.into_pull_up_input(),
+        pins.a5.into_pull_up_input(),
+        50000,
+    );
+
+    let mut buf = [0u8; 8];
 
     loop {
-        led.toggle();
+        I2c::write(&mut i2c, 0x70, &buf).unwrap();
+        for b in buf.iter_mut() {
+            *b = *b ^ 1;
+        }
         arduino_hal::delay_ms(1000);
     }
 }
